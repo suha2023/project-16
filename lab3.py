a7654eb06d75e04088dea583dafadfcd71e25814
suha2023 <150838404+suha2023@users.noreply.github.com>
2023-12-25 23:00:59 +0300

    Add files via upload

---
+++ lab3.py
@@ -0,0 +1,75 @@
+print("Hello W0rld")
+X = 5
+y = "john"
+print(X)
+print(y)
+x = 4
+x = "Sally"
+print(x)
+X = 1
+y = 2.8
+z = 1j
+print(type(X))
+print(type(y))
+print(type(z))
+print(10 + 5)
+x = 3
+y = 10
+z = 3 * 30
+print(z)
+list1 = ["apple", "banana", "cherry"]
+list2 = ["1,5,7,9,3"]
+list3 = [True, False, False]
+print(list1)
+print(list2)
+print(list3)
+print(len(list1))
+a = 33
+b = 200
+if b > a:
+ print("b is greater than a")
+a = 33
+b = 33
+if b > a:
+ print(" b is greater than a")
+elif a==b:
+ print("a and b are equal")
+ a = 200
+ b = 33
+ if b > a:
+  print(" b is greater than a")
+ elif a == b:
+  print("a and b are equal")
+else:
+          print("a is greater than b")
+          i = 1
+          while i < 6:
+            print(i)
+          i += 1
+          i = 1
+          while i < 6:
+           print(i)
+           if i == 3:
+            break
+          i += 1
+i = 0
+while i < 6:
+        i += 1
+        if i == 3:
+          continue
+        print(i)
+
+        i = 1
+
+        while i < 6:
+           print(i)
+           i += 1
+        else:
+         print("i is no longer less than 6")
+
+
+
+
+
+
+
